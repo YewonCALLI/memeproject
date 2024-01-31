277d1fa6e63dcaae53444ab0d7dfac6fb2355c59
designerSejinOH <ohsj1999@naver.com>
2024-01-31 10:41:11 +0900

    Add

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true, // Open to local network and display URL
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist", // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        drawing: resolve(__dirname, "drawing.html"),
+        world: resolve(__dirname, "world.html"),
+      },
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
-}
+  },
+});
