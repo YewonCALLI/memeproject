1e1bc35bb482c06ce73eed547ed88872fcc57424
designerSejinOH <ohsj1999@naver.com>
2024-02-18 19:37:41 +0900

    Add

--- src/imageapi.js
+++ src/imageapi.js
@@ -9,14 +9,17 @@ let query = document.getElementById("firstname").value;
 let button = document.getElementById("button2");
 let result = document.getElementById("result");
 
-button.addEventListener("click", searchPhotos);
-
-function searchPhotos() {
+button.addEventListener("click", () => {
   console.log("search button clicked");
   query = document.getElementById("firstname").value;
-  client.photos.search({ query, per_page: 8 }).then((photos) => {
-    result.innerHTML = photos.photos
-      .map((photo) => `<img src="${photo.src.medium}">`)
-      .join("");
-  });
-}
+  client.photos
+    .search({ query, per_page: 8 })
+    .then((photos) => {
+      result.innerHTML = photos.photos
+        .map((photo) => `<img src="${photo.src.medium}">`)
+        .join("");
+    })
+    .catch((e) => {
+      console.log("error", e);
+    });
+});
